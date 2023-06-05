ba59c0962382a900f7fc303d2fa29e34be2aba79
hari <harikarumuri1086@gmail.com>
2023-06-05 14:23:27 +0530

    new thind uploaded

--- Popularpg/settings.py
+++ Popularpg/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'multiselectfield',
     'rest_framework',
     'drf_yasg',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Popularpg.urls'
--- pgs/urls.py
+++ pgs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from pgs.views import OrderView , ProductListView , ProductDetailView
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     
@@ -7,3 +9,6 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
